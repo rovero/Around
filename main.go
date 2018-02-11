af693c9234c1dae49545a02fa2789ef5a157e7ce
Qianyu Deng <dengqianyu@gmail.com>
2018-02-11 11:02:11 -0800

    add function saveToBT which enables data to be saved in Cloud BigTable

--- main.go
+++ main.go
@@ -7,21 +7,20 @@ import (
       "log"
       "strconv"
       "reflect"
-
        elastic "gopkg.in/olivere/elastic.v3"
         "github.com/pborman/uuid"
-
         "strings"
+        "context"
+        "cloud.google.com/go/bigtable"
+
 )
 
 const (
         INDEX = "around"
         TYPE = "post"
         DISTANCE = "200km"
-        // Needs to update
-        //PROJECT_ID = "around-xxx"
-        //BT_INSTANCE = "around-post"
-        // Needs to update this URL if you deploy it to cloud.
+        PROJECT_ID = "around-xxx"
+        BT_INSTANCE = "around-post"
         ES_URL = "http://35.229.118.55:9200"
 )
 
@@ -90,10 +89,37 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
       }
 
         id := uuid.New()
-        // Save to ES.
+        // Save to ES
         saveToES(&p, id)
+        // Save to BigTable
+        saveToBT(&p, id)
 }
 
+func saveToBT(p *Post, id string) {
+        ctx := context.Background()
+        // you must update project name here
+        bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+        if err != nil {
+                panic(err)
+                return
+        }
+
+        // TODO (student questions) save Post into BT as well
+        tbl := bt_client.Open("post")
+        mut := bigtable.NewMutation()
+        t := bigtable.Now()
+        mut.Set("post", "user", t, []byte(p.User))
+        mut.Set("post", "message", t, []byte(p.Message))
+        mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+        mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+        err = tbl.Apply(ctx, id, mut)
+        if err != nil {
+                panic(err)
+                return
+        }
+        fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+}
 // Save a post to ElasticSearch
 func saveToES(p *Post, id string) {
         // Create a client
@@ -131,7 +157,7 @@ func handlerSearch(w http.ResponseWriter, r *http.Request){
                 ran = val + "km"
         }
 
-        fmt.Printf( "Search received: %f %f %s\n", lat, lon, ran)
+        //fmt.Printf( "Search received: %f %f %s\n", lat, lon, ran)
 
 
         // Create a client
