a0d214ac9c03ccf488a2726dc39bd7e775244cf3
Qianyu Deng <dengqianyu@gmail.com>
2018-02-10 23:31:06 -0800

    change url

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ const (
         //PROJECT_ID = "around-xxx"
         //BT_INSTANCE = "around-post"
         // Needs to update this URL if you deploy it to cloud.
-        ES_URL = "http://35.196.170.56:9200"
+        ES_URL = "http://35.229.118.55:9200"
 )
 
 
